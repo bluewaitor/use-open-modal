e2389ebd56d9104adcd2e481218b12824438885a
blue <405622394@qq.com>
2024-03-13 18:03:08 +0800

    chore(*): export type

--- lib/main.ts
+++ lib/main.ts
@@ -1,2 +1,3 @@
 export { UseOpenModalContextProvider } from "./use-open-modal/context";
+export type { UseOpenModalProps } from "./use-open-modal/types";
 export { useOpenModal } from "./use-open-modal/useOpenModal";
